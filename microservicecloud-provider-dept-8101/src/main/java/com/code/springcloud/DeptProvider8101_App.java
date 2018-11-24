3f18af6d3a2912874a505e59cdbad3fea3c2636e
kabu <15220033993@163.com>
2018-11-24 21:38:54 +0800

    服务提供者、整合mybatis

---
+++ microservicecloud-provider-dept-8101/src/main/java/com/code/springcloud/DeptProvider8101_App.java
@@ -0,0 +1,12 @@
+package com.code.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeptProvider8101_App {
+	public static void main(String[] args)
+	{
+		SpringApplication.run(DeptProvider8101_App.class, args);
+	}
+}
