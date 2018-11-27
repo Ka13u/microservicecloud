7d9007445e2fb33c04a62c5001f805d9d4dab29c
kabu <15220033993@163.com>
2018-11-27 10:44:38 +0800

    Eureka集群配置

---
+++ microservicecloud-eureka-7102/src/main/java/com/code/springcloud/EurekaServer7102_App.java
@@ -0,0 +1,15 @@
+package com.code.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer7102_App
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(EurekaServer7102_App.class, args);
+	}
+}
---
+++ microservicecloud-eureka-7103/src/main/java/com/code/springcloud/EurekaServer7103_App.java
@@ -0,0 +1,15 @@
+package com.code.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer7103_App
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(EurekaServer7103_App.class, args);
+	}
+}
